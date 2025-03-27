fb835ea03b2fd5f6a213b61652f21a0ca02a88af
Goncalo Gabriel <code@grgabriel.eu>
2025-03-27 21:09:20 +0000

    Added nvim from lazyvim, removed tmux

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -1,49 +1,53 @@
--- ~/.config/lua/config/lazy.lua
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
-vim.g.mapleader = " " -- the leader key is used in many keymaps, 
-
-local plugins = {
-    "nvim-lua/plenary.nvim",
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-    {"nvim-telescope/telescope.nvim", tag = '0.1.6', 
-        requires = { {"nvim-lua/plenary.nvim"}}},
-    {"ThePrimeagen/harpoon", branch = "harpoon2",
-        dependencies = {"nvim-lua/plenary.nvim"}},
-    {"mbbill/undotree"},
-    {"tpope/vim-fugitive"},
-    --lsp configuration
-        {"neovim/nvim-lspconfig"}, --lsp configs
-        {"hrsh7th/cmp-nvim-lsp"}, -- autocompletion
-        {"hrsh7th/nvim-cmp"}, --additional autocompletion
-        {"L3MON4D3/LuaSnip", version = "v2.*", build = "make install_jsregexp", dependencies = {'saadparwaiz1/cmp_luasnip','rafamadriz/friendly-snippets'}}, --snippet engine
-        {"williamboman/mason.nvim"}, --lsp package manager
-        {"williamboman/mason-lspconfig.nvim"}, --lsp package manager configs
-    --color scheme 
-    {'catppuccin/nvim'}, 
-    -- for Go programming
-    {
-       "ray-x/go.nvim",
-       dependencies = {"ray-x/guihua.lua"},
-       config = function()
-         require("go").setup()
-       end,
-       event = {"CmdlineEnter"},
-       ft = {"go", 'gomod'},
-       lazy = false,
-    }
-}
-
-require("lazy").setup(plugins, {})
+require("lazy").setup({
+  spec = {
+    -- add LazyVim and import its plugins
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- import/override with your plugins
+    { import = "plugins" },
+  },
+  defaults = {
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
+  },
+  install = { colorscheme = { "tokyonight", "habamax" } },
+  checker = {
+    enabled = true, -- check for plugin updates periodically
+    notify = false, -- notify on update
+  }, -- automatically check for plugin updates
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
